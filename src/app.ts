45edfffe1fd3bba38a1bb64097fee1bd0ae8bda5
Ahmed <sultan_902002@yahoo.com>
2020-02-19 03:01:44 -0700

    set view engine to use html and create class constructor fot the json object

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,40 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Request, Response } from "express";
+import bodyParser from "body-parser";
+import { stringify } from "querystring";
 export interface ProcessEnv {
-  [key: string]: string | undefined
+  [key: string]: string | undefined;
 }
 
 const app: Application = express();
-const ENV = process.env.NODE_ENV || "development";
+const ENV: string = process.env.NODE_ENV || "development";
 const PORT: number = parseInt(`${process.env.PORT}`, 10) || 8001;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('hello');
+app.use(express.json());
+app.use(express.static("../library"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "pug");
+
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile("index.html", { root: __dirname });
 });
 
+app.post("/submit", (req: Request, res: Response) => {
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT} in ${ENV} mode.`));
+  class Result {
+    imageName: string = req.body.imageName;
+    annotations: {
+      annotationID: string;
+      upperLeft: { pointID: string; position: [number, number] };
+      lowerRight: { pointID: string; position: [number, number] };
+      type: string;
+    }[] = req.body.annotations;
+};
+
+const result = new Result;
+
+  res.json(result);
+});
+
+app.listen(PORT, () =>
+  console.log(`Listening on port ${PORT} in ${ENV} mode.`)
+);
