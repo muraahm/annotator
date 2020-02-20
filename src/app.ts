fc51d2056c003e433f6cea18fcc1eef18e0d3ec7
Ahmed <sultan_902002@yahoo.com>
2020-02-20 00:57:39 -0700

    get the response ready for the backend

--- src/app.ts
+++ src/app.ts
@@ -7,38 +7,35 @@ export interface ProcessEnv {
 }
 
 // Copy all the view templates
-shell.cp( "-R", "src/views", "dist/" );
+shell.cp("-R", "src/views", "dist/");
 
 const app: Application = express();
 const ENV: string = process.env.NODE_ENV || "development";
 const PORT: number = parseInt(`${process.env.PORT}`, 10) || 8001;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname,"views")));
+app.use(express.static(path.join(__dirname, "views")));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "pug");
 
-
-
 app.get("/", (req: Request, res: Response) => {
-  res.sendFile("index.html", { root: __dirname + "/views"});
+  res.sendFile("index.html", { root: __dirname + "/views" });
 });
 
 app.post("/submit", (req: Request, res: Response) => {
+  const imageName: string = req.body.imageName;
+  const annotations: {
+    annotationID: string;
+    upperLeft: { pointID: string; position: [number, number] };
+    lowerRight: { pointID: string; position: [number, number] };
+    type: string;
+  }[] = req.body.annotations;
 
-const imageName: string = req.body.imageName;
-const annotations: {
-        annotationID: string,
-        upperLeft: { pointID: string; position: [number, number] },
-        lowerRight: { pointID: string; position: [number, number] },
-        type: string
-      }[] = req.body.annotations; 
-
-const result = {
-  imageName, annotations
-}
-
-
+  const result = {
+    imageName,
+    annotations
+  };
+  console.log(result);
   res.json(result);
 });
 
