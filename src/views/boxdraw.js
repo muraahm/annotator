695ef2daa59bfc00bef6f398b91bca8190d4ece0
Ahmed <sultan_902002@yahoo.com>
2020-02-20 00:58:09 -0700

    add interesting functunality

--- src/views/boxdraw.js
+++ src/views/boxdraw.js
@@ -11,7 +11,29 @@ $(() => {
   let mouseIsDown = 0;
   let squares = [];
   let annotations = squares;
+  let interesting = []
 
+
+  $('#interesting').on('click', function () {
+    $('#uninteresting').removeClass('uninteresting');
+    $(this).addClass('interesting');
+    interesting.push("Interesting")
+  });
+  $('#uninteresting').on('click', function () {
+    $('#interesting').removeClass('interesting');
+    $(this).addClass('uninteresting');
+    interesting.push("Uninteresting")
+  });
+
+  function generateUID() {
+    // I generate the UID from two parts here 
+    // to ensure the random number provide enough bits.
+    var firstPart = (Math.random() * 46656) | 0;
+    var secondPart = (Math.random() * 46656) | 0;
+    firstPart = ("000" + firstPart.toString(36)).slice(-3);
+    secondPart = ("000" + secondPart.toString(36)).slice(-3);
+    return firstPart + secondPart;
+  }
 
   function getMousePos(canvas, evt) {
     let rect = canvas.getBoundingClientRect();
@@ -28,21 +50,35 @@ $(() => {
     context.clearRect(0, 0, context.width, context.height);
     //or use fillRect if you use a bg color
 
-    context.beginPath();
-    context.rect(startX, startY, width, height);
-    context.fillStyle = "yellow";
-    context.fill();
-    // context.fillStyle = "rgba(255, 255, 255, 0.5)";
-    // context.globalCompositeOperation = "lighter";
-    context.lineWidth = 7;
-    context.strokeStyle = 'black';
-    context.stroke();
+    // context.beginPath();
+    // context.rect(startX, startY, width, height);
+    // context.fillStyle = "yellow";
+    // context.canvas.addEventListener('click', function(e) {
+    //     let mouseX = e.clientX - context.canvas.offsetLeft
+    //     let mouseY = e.clientY - context.canvas.offsetTop
+
+    //     context.fillStyle = "orange"
+    //     context.globalAlpha = 0.2;
+    //     context.fillRect(mouseX, mouseY, 30,30)
+    // })
+
+    context.fillRect(startX, startY, width, height);
+    context.fillStyle = "orange"
+
+
+
+
+    // context.globalAlpha = 1.0;
+    // context.lineWidth = 1;
+    // context.strokeStyle = 'black';
+    // context.stroke();
 
     if (mouseIsDown === 0) {
-      let upperLeft = { pointonId: '1', position: [startX, startY] }
-      let lowerRight = { pointId: '1', position: [endX, endY] }
-      let annotation = { annotationID: '1', upperLeft, lowerRight, type: 'ineristing' }
+      let upperLeft = { pointId: generateUID(), position: [startX, startY] }
+      let lowerRight = { pointId: generateUID(), position: [endX, endY] }
+      let annotation = { annotationID: generateUID(), upperLeft, lowerRight, type: interesting[0] }
       squares.push(annotation)
+      interesting = [];
     }
   }
 
@@ -62,6 +98,7 @@ $(() => {
     startX = endX = pos.x;
     startY = endY = pos.y;
     drawSquare(); //update
+
   }
 
   function mouseXY(eve) {
@@ -79,15 +116,18 @@ $(() => {
   $('#JSONOutput').click(() => {
     let imageElement = document.getElementById('imgupload');
     let imageName = imageElement.value.replace(/.*[\/\\]/, '')
-    
-    $.post("/submit",
-      {
-        lable: "submit",
-        content: { imageName, annotations }
-      },
-      function (data, status) {
-        result.value = status + ":" + data;
+
+    $.ajax({
+      url: '/submit',
+      type: 'post',
+      dataType: 'json',
+      contentType: 'application/json',
+      success: function (data) {
         console.log(data)
-      })
+      },
+      data: JSON.stringify({ imageName: imageName, annotations: annotations })
+    });
+
+
   });
 })
